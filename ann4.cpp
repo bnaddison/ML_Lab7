0c264aaf9e9cc95f6fe620e17a28ea4646d5b4fe
Brandon Addison <addbra001@myuct.ac.za>
2018-10-01 01:23:56 +0200

    fixed bug with mse

--- ann4.cpp
+++ ann4.cpp
@@ -59,11 +59,10 @@ float output_node_error(float o, float t) {
   return e;
 }
 
-float hidden_node_error(float o, vector< pair<float, float> > we ) {
-  float error_sum = 0;
-  for (int i = 0; i < we.size(); i++) {
-    error_sum += we[i].first*we[i].second;
-  }
+float hidden_node_error(float o, pair<float, float> we ) {
+  //float error_sum = 0;
+  float error_sum = we.first*we.second;
+
   float e = o*(1-o)*error_sum;
 
   return e;
@@ -71,12 +70,13 @@ float hidden_node_error(float o, vector< pair<float, float> > we ) {
 
 float random_float() {
   float r = (float)rand()/(float)RAND_MAX;
-  float ans = r * 0.8f;
-  return r + 0.2f;
+  float ans = r * 2;
+  return -1 + ans;
 }
 
 
 int main () {
+  int count = 0;
 
   float w11 = random_float();
   float w12 = random_float();
@@ -90,36 +90,98 @@ int main () {
   float w32 = random_float();
   float w33 = random_float();
 
-  cout << w11 << " " << w12 << " " << w13 << " " << w21 << " " << w22 << " " << w23 << endl;
+  float v1 = random_float();
+  float v2 = random_float();
+  float v3 = random_float();
+
+  float n = 0.1f;
+  float mse = 1;
+
+  //cout << w11 << " " << w12 << " " << w13 << " " << w21 << " " << w22 << " " << w23 << endl;
   readfile("training examples");
+  while (mse > 0.001f) {
+    float temp_mse = 0;
+    cout << "ITERATION: " << count << endl;
   for (int i = 0; i < training_examples.size(); i++) {
-    cout << training_examples[i][0] << " " << training_examples[i][1] << " " << training_examples[i][2] << " " << training_examples[i][3] << endl;
+  //  cout << training_examples[i][0] << " " << training_examples[i][1] << " " << training_examples[i][2] << " " << training_examples[i][3] << endl;
 
     pair <float, float> x1n1(training_examples[i][0],w11);
     pair <float, float> x2n1(training_examples[i][1],w21);
     pair <float, float> x3n1(training_examples[i][2],w31);
 
+    vector<pair<float,float> > node1;
     node1.push_back(x1n1);
     node1.push_back(x2n1);
     node1.push_back(x3n1);
-
     float n1 = node_output(node1);
 
     pair <float, float> x1n2(training_examples[i][0],w12);
     pair <float, float> x2n2(training_examples[i][1],w22);
     pair <float, float> x3n2(training_examples[i][2],w32);
 
+    vector<pair<float,float> > node2;
     node2.push_back(x1n2);
     node2.push_back(x2n2);
     node2.push_back(x3n2);
-    float n1 = node_output(node2);
+    float n2 = node_output(node2);
 
     pair <float, float> x1n3(training_examples[i][0],w13);
     pair <float, float> x2n3(training_examples[i][1],w23);
     pair <float, float> x3n3(training_examples[i][2],w33);
 
-    node.push_back(x1n1);
-    node.push_back(x2n1);
-    float n1 = node_output(node);
+    vector<pair<float,float> > node3;
+    node3.push_back(x1n3);
+    node3.push_back(x2n3);
+    node3.push_back(x3n3);
+    float n3 = node_output(node3);
+
+    pair <float, float> n1o(n1,v1);
+    pair <float, float> n2o(n2,v2);
+    pair <float, float> n3o(n3,v3);
+
+    vector<pair<float,float> > o_node;
+
+    o_node.push_back(n1o);
+    o_node.push_back(n2o);
+    o_node.push_back(n3o);
+    float output = node_output(o_node);
+    cout << "Output: " << output <<  endl << "Target Output: " << training_examples[i][3] << endl;
+
+    float o_error = output_node_error(output,training_examples[i][3]);
+
+    cout << "Output Error: " << o_error << endl << endl;
+
+    pair <float,float> v1o(v1,o_error);
+    pair <float,float> v2o(v2,o_error);
+    pair <float,float> v3o(v3,o_error);
+
+    float n1_error = hidden_node_error(n1, v1o);
+    float n2_error = hidden_node_error(n2, v2o);
+    float n3_error = hidden_node_error(n3, v3o);
+    //node.clear();
+
+    v1+= n*o_error*n1;
+    v2+= n*o_error*n2;
+    v3+= n*o_error*n3;
+    //cout << "v1: " << v1 << endl << "v2: " << v2 << endl << "v3: " << v3 << endl;
+
+    //cout << "Hidden Node 1 Error: " << n1_error << endl << "Hidden Node 2 Error: " << n2_error << endl;
+
+    w11+= n*n1_error*training_examples[i][0];
+    w12+= n*n2_error*training_examples[i][0];
+    w13+= n*n3_error*training_examples[i][0];
+    w21+= n*n1_error*training_examples[i][1];
+    w22+= n*n2_error*training_examples[i][1];
+    w23+= n*n3_error*training_examples[i][1];
+    w31+= n*n1_error*training_examples[i][2];
+    w32+= n*n2_error*training_examples[i][2];
+    w33+= n*n3_error*training_examples[i][2];
+
+    //cout << "w11: " << w11 << endl << "w12: " << w12 << endl <<  "w13: " << w13 << endl << "w21: " << w21 << "w22: " << w22 << "w23: " << w23 << "w31: " << w31 << "w32: " << w32 << "w33: " << w33 << endl;
+    temp_mse += pow((training_examples[i][3] - output),2);
   }
+  mse = temp_mse/training_examples.size();
+  cout << "MSE: " << mse << endl;
+  count++;
+}
 }
